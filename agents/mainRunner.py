5267901a345f1ac68991cdfe1544066c55b5fbe9
Honghui-Li-8 <honghuili8@gmail.com>
2024-10-20 02:32:25 -0700

    communication btw agents

---
+++ agents/mainRunner.py
@@ -0,0 +1,36 @@
+import json
+from uagents import Agent, Bureau, Context, Model
+from textToAudioAgent import textToAudioAgent
+
+
+class Message(Model):
+    jsonStr: str
+    
+starUpAgent = Agent(
+  name="startUp",
+  port="8180",
+  seed="dasdafs",
+  endpoint=["http://127.0.0.1:8180/submit"]
+)
+@starUpAgent.on_event("startup")
+async def test_agents(ctx: Context):
+  data = {
+    "transcript":"茉莉花",
+    "language":"zh"
+  }
+  print("-- send to textToAudio")
+  data_jsonStr = json.dumps(data)
+  RECIPIENT_ADDRESS = (
+    f"test-agent://{textToAudioAgent.address}"
+  )
+  response = await ctx.send(RECIPIENT_ADDRESS, Message(jsonStr=data_jsonStr))
+  
+  print(f"response:{response}")
+
+print(textToAudioAgent.address)
+bureau = Bureau()
+bureau.add(starUpAgent)
+bureau.add(textToAudioAgent)
+ 
+if __name__ == "__main__":
+    bureau.run()
---
+++ agents/textToAudioAgent.py
@@ -0,0 +1,54 @@
+import json
+from cartesiaTextToSpeech import cartesiaTextToSpeech
+from uagents import Agent, Context, Model
+
+class Message(Model):
+    jsonStr: str
+
+class Response(Model):
+    jsonStr: str
+    errorStr: str
+
+textToAudioPort = 8083
+textToAudioAgent = Agent(
+  name="textToAudio",
+  port=textToAudioPort,
+  seed="callhack-11.0-wdljs-textToAudio",
+  endpoint=[f"http://127.0.0.1:{str(textToAudioPort)}/submit"]
+)
+
+
+def parseJson(jsonStr):
+    try:
+        # Attempt to parse the JSON string
+        parsed_data = json.loads(jsonStr)
+        return parsed_data
+    except json.JSONDecodeError as e:
+        # Handle parsing error, such as malformed JSON
+        print(f"Failed to parse JSON: {e}")
+        return None
+
+@textToAudioAgent.on_event("startup")
+async def starUp_printing(ctx: Context):
+    ctx.logger.info(f"textToAudio agent running, address:{textToAudioAgent.address}")
+  
+@textToAudioAgent.on_message(model=Message)
+async def textToAudio_message_handler(ctx: Context, sender: str, msg: Message):
+    data = parseJson(msg.jsonStr)
+    
+    # input validation
+    errorInput = False
+    if data == None:
+        errorInput = True
+    elif "transcript" not in data or "language" not in data:
+        errorInput = True
+    elif not isinstance(data["transcript"], str) or not isinstance(data["language"], str):
+        errorInput = True
+      
+    if errorInput:
+        await ctx.send(sender, Response(jsonStr="", errorStr="Error Input"))
+        return
+    
+    # correct input
+    cartesiaTextToSpeech(data["transcript"], data["language"])
+    
